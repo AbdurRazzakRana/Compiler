9490039849cf38b1b03c5a9b7912f0ebe4dc908f
AbdurRazzakRana <razzakrana17@gmail.com>
2024-05-01 21:40:26 -0600

    Up Until now read write in arrays works

--- lab9/test.c
+++ lab9/test.c
@@ -1,26 +1,23 @@
+// program reads in several values and
+// the prints the square of them out
 void main(void) {
-  int i,x;
-   i=10;
-   write i+1;
-   write "\n";
-   write i-1;
-   write "\n" ;
-   write i*5 ;
-   write "\n" ;
-   write i/5 ;
-   write "\n" ;
-   write i%5 ;
-   write "\n" ;
-   write i<=1 ;
-   write "\n" ;
-   write i<1 ;
-   write "\n" ;
-   write i>1 ;
-   write "\n" ;
-   write i>=1 ;
-   write "\n" ;
-   write i==1 ;
-   write "\n" ;
-   write i!=1 ;
-   write "\n" ;
+  int x[5];
+  int i;
+   i=0;
+  while (i < 5 )
+   {
+      write "enter X ";
+      read x[i];
+      i = i + 1;
+   }
+ 
+   i=0;
+
+  while (i < 5)
+   {
+      write x[i] * x[i];
+      write "\n";
+      i = i + 1;
+   }
+
 }
